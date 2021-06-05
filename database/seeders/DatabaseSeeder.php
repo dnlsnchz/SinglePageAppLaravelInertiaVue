5051b9e22517bf5144fb809ad4db1e1a8b4b80df
Daniel Sanchez <dnlsnchz4@gmail.com>
2021-06-05 00:43:15 -0500

    Crea proyecto se agrega jetstream y modelo de note.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::Factory()->create([
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();
+    }
+}
